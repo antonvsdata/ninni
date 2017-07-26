b5cac29867504f58fce75f9dc0a669ad141c0c33
antom <antom@uef.fi>
2017-07-26 11:01:30 +0300

    Sepecified package versions

--- src/install.packages.R
+++ src/install.packages.R
@@ -1,14 +1,22 @@
-packages <- c("shiny",
-              "DT",
-              "DBI",
-              "tidyr",
-              "RPostgreSQL",
-              "dbplyr")
+# Install devtools package to be able to install specific versions of packages
+install.packages("devtools")
 
-install.packages(packages)
+# List packages and specify versions
+packages <- list("shiny" = "1.0.3",
+              "plotly" = "4.6.0",
+              "DT"  = "0.2",
+              "DBI" = "0.7",
+              "tidyr" = "0.6.3",
+              "RPostgreSQL" = "0.6-2",
+              "dbplyr" = "1.1.0")
 
-# Specified versions of certain packages are required for Ninni to work
+# Install packages
+for(package.name in names(packages)){
+  print(paste("Installing package:", package.name, "version:", packages[[package.name]]))
+  devtools::install_version(package.name, version = packages[[package.name]])
+}
+
+# Dev versions of certain packages are required for Ninni to work
 devtools::install_github("rstudio/pool")
-devtools::install_github('hadley/ggplot2')
+devtools::install_github("hadley/ggplot2")
 devtools::install_github("tidyverse/dplyr")
-devtools::install_version("plotly",version="4.6.0")
